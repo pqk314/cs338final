c259ab779a4c5694ada83e623aa74c0531bc6f54
Bryce <bryce.h.bernstein@gmail.com>
2023-10-04 10:28:41 -0500

    starter JS work for buying cards

---
+++ buying-card.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".card").forEach(element => {
+    element.addEventListener("click", () => cardBought(element))
+})
+
+function cardBought(card) {
+    window.location.href =`/cardbought/${card.id}`
+}
