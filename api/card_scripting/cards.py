45895fe615f60091b2edcfccfcfa55308d0e119f
Brendan <banfieldb@carleton.edu>
2023-11-20 01:45:21 -0600

    implemented moat and text display, cleaned up code, and added minor docstrings to cardParser

--- api/card_scripting/cards.py
+++ api/card_scripting/cards.py
@@ -1,9 +1,9 @@
 macros = {
-    'cantrip': '#draw(1); #changeActions(1);',
-    'chapel': 'x=#fromHand(4, T); #trash($x);',
-    'resetText': '#setText(Left click a card to play it.); '
+    'cantrip': '#draw(1); #changeActions(1)',
+    'resetText': '#setText(Left click a card to play it.)'
 }
 # use '&macroname' in a card to call the macro
+
 supply_options = [
     'cellar',
     'chapel',
@@ -32,6 +32,7 @@ supply_options = [
     'artisan',
     'gardens',
 ]
+
 cards = {
     'cellar': {'type': 'action', 'cost': 2},
     'chapel': {'type': 'action', 'cost': 2},
@@ -59,7 +60,6 @@ cards = {
     'witch': {'type': 'action', 'cost': 5},
     'artisan': {'type': 'action', 'cost': 6},
 
-
     'copper': {'type': 'treasure', 'cost': 0},
     'silver': {'type': 'treasure', 'cost': 3},
     'gold': {'type': 'treasure', 'cost': 6},
@@ -77,29 +77,29 @@ cardTexts = {
     'cellar': '#changeActions(1); #setText(Choose any number of cards to discard); x=#chooseSubset(#getHand(), -1, #true()); #discard($x); #draw(#count($x)); &resetText',
     'chapel': '#setText(Choose up to 4 cards to trash); #trash(#chooseSubset(#getHand(), 4, #true())); &resetText',
     'moat': '#draw(2)',
-    'harbinger': '#draw(1); #changeActions(1); #setText(Choose up to 1 card to put on top of your deck); #toDeck(#chooseSubset(#getDiscard(), 1, #true()))',
-    'merchant': '#draw(1); #changeActions(1); #merchant()',
+    'harbinger': '&cantrip; #setText(Choose up to 1 card to put on top of your deck); #toDeck(#chooseSubset(#getDiscard(), 1, #true())); &resetText',
+    'merchant': '&cantrip; #merchant()',
     'vassal': '#changeCoins(2); #setText(Choose up to 1 action to play); x=#getFirst(#fromTop(1)); #discard($x); actions=#getSubset($x, #makeArray(type, =, action)); toPlay=#chooseSubset($actions, 1, #true()); #cond(#eval(#count($toPlay), >, 0), #play($toPlay)); #cond(#eval(#count($toPlay), >, 0), #execute(#getFirst($toPlay))); &resetText',
     'village': '#draw(1); #changeActions(2)',
     'workshop': '#setText(Choose a card to gain); card=#chooseSubset(#getSubset(#getStore(), #makeArray(cost, <, 5)), 1, #false()); #gain($card); #decreaseSupply($card); &resetText',
-    'bureaucrat': '#gain(#fromStore(silver), deck); #attack(`#setText(Choose a victory card to put on top of your deck); #toDeck(#chooseSubset(#getSubset(#getHand(), #makeArray(type, =, victory)), 1, #false()))`)',
-    'militia': '#changeCoins(2); #attack(`x=#eval(#count(#getHand()), -, 3); #cond(#eval($x, >, 0), #set(toDiscard, #chooseSubset(#getHand(), $x, #false()))); #cond(#eval($x, >, 0), #discard($toDiscard))`)',
-    'moneylender': 'x=#getSubset(#getHand(), #makeArray(name, =, copper)); toTrash=#chooseSubset($x, 1, #true()); willTrash=#false(); #cond(#eval(#count($toTrash), >, 0), willTrash=#true()); #cond($willTrash, #trash($toTrash)); #cond($willTrash, #changeCoins(3))',
-    'poacher': '#draw(1); #changeActions(1); #changeCoins(1); #discard(#chooseSubset(#getHand(), #eval(17, -, #count(#getStore())), #false()))',
-    'remodel': 'x=#chooseSubset(#getHand(), 1, #false()); cost=#getCost(#getFirst($x)); #trash($x); options=#getSubset(#getStore(), #makeArray(cost, <=, #eval($cost, +, 2))); card=#chooseSubset($options, 1, #false()); #gain($card); #decreaseSupply($card)',
+    'bureaucrat': '#gain(#fromStore(silver), deck); #attack(`#setText(Choose a victory card to put on top of your deck); options=#getSubset(#getHand(), #makeArray(type, =, victory)); #toDeck(#chooseSubset($options, 1, #false())); #cond(#eval(#count($options), = , 0), #reveal(#getHand(), Player reveals their hand with no victory cards:))`)',
+    'militia': '#changeCoins(2); #attack(`x=#eval(#count(#getHand()), -, 3); #setText(Choose {} cards to discard, $x); #cond(#eval($x, >, 0), #set(toDiscard, #chooseSubset(#getHand(), $x, #false()))); #cond(#eval($x, >, 0), #discard($toDiscard)); #cond(#eval($x, >, 0), #reveal($toDiscard, Player discarded:)); &resetText; `)',
+    'moneylender': 'x=#getSubset(#getHand(), #makeArray(name, =, copper)); #setText(Choose up to one Copper to trash); toTrash=#chooseSubset($x, 1, #true()); willTrash=#false(); #cond(#eval(#count($toTrash), >, 0), willTrash=#true()); #cond($willTrash, #trash($toTrash)); #cond($willTrash, #changeCoins(3)); &resetText',
+    'poacher': '&cantrip; #changeCoins(1); x=#eval(17, -, #count(#getStore())); #setText(Choose {} cards to discard, $x); #discard(#chooseSubset(#getHand(), $x, #false())); &resetText',
+    'remodel': '#setText(Choose a card to trash); x=#chooseSubset(#getHand(), 1, #false()); cost=#getCost(#getFirst($x)); #trash($x); options=#getSubset(#getStore(), #makeArray(cost, <=, #eval($cost, +, 2))); #setText(Choose a card to gain); card=#chooseSubset($options, 1, #false()); #gain($card); #decreaseSupply($card); &resetText',
     'smithy': '#draw(3)',
-    'throne_room': 'c=#chooseSubset(#getSubset(#getHand(), #makeArray(type, =, action)), 1, #true()); #play($c); #execute($c); #execute($c)',
-    'bandit': '#gain(#fromStore(gold)); #attack(`top=#fromTop(2); toTrash=#chooseSubset(#getSubset(#getSubset($top, #makeArray(type, =, treasure)), #makeArray(name, !=, copper)), 1, #false()); #discard($top); #trash($toTrash)`)',
+    'throne_room': '#setText(Choose up to one action to play); c=#chooseSubset(#getSubset(#getHand(), #makeArray(type, =, action)), 1, #true()); #play($c); &resetText; #execute($c); #execute($c)',
+    'bandit': '#gain(#fromStore(gold)); #attack(`top=#fromTop(2); #reveal($top, Player discarded:); #setText(Choose a treasure card besides a Copper to trash); toTrash=#chooseSubset(#getSubset(#getSubset($top, #makeArray(type, =, treasure)), #makeArray(name, !=, copper)), 1, #false()); #discard($top); #trash($toTrash); &resetText; `)',
     'council_room': '#draw(4); #changeBuys(1); #attack(`#draw(1)`)',
     'festival': '#changeActions(2); #changeBuys(1); #changeCoins(2)',
     'laboratory': '#draw(2); #changeActions(1)',
-    'library': 'cards=#fromTop(1); break=#eval(#eval(#count(#getHand()), >=, 7), or, #eval(#count($cards), =, 0)); #cond($break, #discard(#getSetAside())); #cond($break, #endEarly()); actions=#getSubset($cards, #makeArray(type, =, action)); skip=#chooseSubset($actions, -1, #true()); #toHand($cards); #setAside($skip); #execute(#makeCard(library));', #not implemented
-    'market': '#draw(1); #changeActions(1); #changeBuys(1); #changeCoins(1)',
+    'library': 'cards=#fromTop(1); break=#eval(#eval(#count(#getHand()), >=, 7), or, #eval(#count($cards), =, 0)); #cond($break, #discard(#getSetAside())); #cond($break, #endEarly()); actions=#getSubset($cards, #makeArray(type, =, action)); #setText(Choose up to one action to set aside); skip=#chooseSubset($actions, -1, #true()); &resetText; #toHand($cards); #setAside($skip); #execute(#makeCard(library));',
+    'market': '&cantrip; #changeBuys(1); #changeCoins(1)',
     
-    'mine':'x=#chooseSubset(#getSubset(#getHand(), #makeArray(type, =, treasure)), 1, #false()); cost=#getCost(#getFirst($x)); #trash($x); options=#getSubset(#getStore(), #makeArray(cost, <=, #eval($cost, +, 3)), #makeArray(type, =, treasure)); card=#chooseSubset($options, 1, #false()); #gain($card, hand); #decreaseSupply($card);',
-    'sentry': '#draw(1); #changeActions(1); x=#fromTop(2); toTrash=#chooseSubset($x, 2, #true()); #trash($toTrash); x=#removeFromSet($x, $toTrash); toDiscard=#chooseSubset($x, 2, #true()); #discard($toDiscard); x=#removeFromSet($x, $toDiscard); #toDeck($x); x=#chooseSubset($x, 1, #false()); #toDeck($x)',
+    'mine':'#setText(Choose up to one treasure to trash); x=#chooseSubset(#getSubset(#getHand(), #makeArray(type, =, treasure)), 1, #false()); cost=#getCost(#getFirst($x)); #trash($x); options=#getSubset(#getStore(), #makeArray(cost, <=, #eval($cost, +, 3)), #makeArray(type, =, treasure)); #setText(Choose a treasure to gain to your hand); card=#chooseSubset($options, 1, #false()); #gain($card, hand); #decreaseSupply($card); &resetText',
+    'sentry': '&cantrip; x=#fromTop(2); #setText(Choose any number of cards to trash); toTrash=#chooseSubset($x, 2, #true()); #trash($toTrash); x=#removeFromSet($x, $toTrash); #setText(Choose any number of cards to discard); toDiscard=#chooseSubset($x, 2, #true()); #discard($toDiscard); x=#removeFromSet($x, $toDiscard); #toDeck($x); #setText(Choose a card to have on top of your deck); x=#chooseSubset($x, 1, #false()); #toDeck($x); &resetText',
     'witch': '#draw(2); #attack(`curse=#fromStore(curse); #gain($curse)`)',
-    'artisan': 'card=#chooseSubset(#getSubset(#getStore(), #makeArray(cost, <, 6)), 1, #false()); #gain($card, hand); #decreaseSupply($card); #toDeck(#chooseSubset(#getHand(), 1, #false()))',
+    'artisan': '#setText(Choose a card to gain to your hand); card=#chooseSubset(#getSubset(#getStore(), #makeArray(cost, <, 6)), 1, #false()); #gain($card, hand); #decreaseSupply($card); #setText(Choose a card to put on top of your deck); #toDeck(#chooseSubset(#getHand(), 1, #false())); &resetText',
 
 
     'copper': '#changeCoins(1)',
--- api/card_scripting/commands.py
+++ api/card_scripting/commands.py
@@ -70,8 +70,10 @@ def getDiscard(args, player):
 
 def getSetAside(args, player):
     # no args
-    # returns list of all cards in discard pile
-    return player.set_aside
+    # returns list of all cards that were set aside, then clears the list
+    res = player.set_aside
+    player.set_aside = []
+    return res
 
 def fromTop(args, player):
     # args: number
@@ -187,7 +189,8 @@ def count(args, player):
 def attack(args, player):
     # args: multicommand string
     # executes the multicommand for each player besides the current player
-    cmd = args[0]
+    moatText = "#setText(Reveal a moat to be unaffected by attack?); moat=#chooseSubset(#getFirst(#getSubset(#getHand(), #makeArray(name, =, moat))), 1, #true()); #cond(#eval(#count($moat), >, 0), #reveal($moat, Player reveals a moat)); #cond(#eval(#count($moat), >, 0), #endEarly()); "
+    cmd = moatText + args[0]
     shouldYield = False
     for p in player.game.players:
         if p is player:
@@ -242,6 +245,8 @@ def getName(args, player):
 
 def getCost(args, player):
     # args: card
+    if args[0] == []:
+        return -1
     return args[0]['cost']
 
 def getType(args, player):
@@ -313,10 +318,24 @@ def setText(args, player):
 
         player.set_text(args[0].format(*values))
 
+def reveal(args, player):
+    # args: cards, text
+    if type(args[0]) != list:
+        args[0] = [args[0]]
+    names = [card['name'] for card in args[0]]
+    for p in player.game.players:
+        if p == player:
+            continue
+        p.set_text(args[1])
+        for name in names:
+            p.update_list('reveal', name)
+
 def chooseSubset(args, player):
     # args: set, n, canChooseLess
     # Asks the player to choose a subset of the set (list of cards), of size n, with the possible option to choose less than n
     # returns a list of the chosen cards
+    if not type(args[0]) == list:
+        args[0] = [args[0]]
     o = {'options': args[0], 'n': int(args[1]), 'canChooseLess': args[2]}
     if o['n'] >= len(o['options']) and not o['canChooseLess']:
         return o['options']
@@ -385,7 +404,7 @@ def eval(args, player):
     
 
 
-funcs = [getHand, getDiscard, getSetAside, fromTop, getStore, fromStore, decreaseSupply, merchant, gain, trash, play, toHand, discard, toDeck, setAside, changeCoins, changeBuys, changeActions, draw, count, getChoice, getName, getCost, getType, getFirst, getSubset, chooseSubset, removeFromSet, true, false, eval, makeArray, attack, execute, makeCard, endEarly]
+funcs = [getHand, getDiscard, getSetAside, fromTop, getStore, fromStore, decreaseSupply, merchant, gain, trash, play, toHand, discard, toDeck, setAside, changeCoins, changeBuys, changeActions, draw, count, getChoice, getName, getCost, getType, getFirst, getSubset, setText, reveal, chooseSubset, removeFromSet, true, false, eval, makeArray, attack, execute, makeCard, endEarly]
 
 yieldFuncs = ['fromHand', 'getChoice', 'chooseSubset']
 commands = {}
